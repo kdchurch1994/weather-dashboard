f7e7d48b314e189fea996d9c280c325aefd431b0
kdchurch1994 <kdchurch@bsu.edu>
2021-08-22 10:31:32 -0400

    added moment.js to include current date and add const for storing and displaying weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-// // Using Moment JS to Display the current date and update the current date
-// var todaysDate = null; //sets the variable todaysDate to null. Will be called in a function. 
-// var currentTime = null; // sets the variable currentTime to null. Will be called in a function
+// Using Moment JS to Display the current date and update the current date
+var todaysDate = null; //sets the variable todaysDate to null. Will be called in a function. 
+var currentTime = null; // sets the variable currentTime to null. Will be called in a function
 
-// //Update function that allows us to set a date format using moment.js.
-// var update = function () { 
-//     todaysDate = moment (new Date ())
-//     $("#currentDay").html(todaysDate.format ('dddd, MMMM Do, YYYY: hh:mm:ss a')); //Writes the current date to the html page in the format of day of the week, month, date of the month (1-31), year, hour, minute, and second in that order)
-// };
+//Update function that allows us to set a date format using moment.js.
+var update = function () { 
+    todaysDate = moment (new Date ())
+    $("#currentDay").html(todaysDate.format ('dddd, MMMM Do, YYYY: hh:mm:ss a')); //Writes the current date to the html page in the format of day of the week, month, date of the month (1-31), year, hour, minute, and second in that order)
+};
 
-// //Function used to update the time every second. The time updates with real time information
-// $(document).ready(function(){
-//     currentTime = $('#currentDay')
-//     update();
-//     setInterval(update, 1000);
-// });
+//Function used to update the time every second. The time updates with real time information
+$(document).ready(function(){
+    currentTime = $('#currentDay')
+    update();
+    setInterval(update, 1000);
+});
 
 const cityInput = document.getElementById("input-city");
 const citySearchEl = document.getElementById("btn-search");
