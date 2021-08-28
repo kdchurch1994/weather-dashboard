d825f89a4505333161e256082ffbe594c12f8762
kdchurch1994 <kdchurch@bsu.edu>
2021-08-28 12:33:13 -0400

    updates to read-me and comments

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # weather-dashboard
+
+## Purpose
+This website provides the weather forecast based off the name of the city being searched. Once a city is entered, the current day's forecast is provided as well as the five day forecast. The user can for the weather for multiple cities, and previous cities will be able to be viewed if selected.
+
+## Built with
+    * HTML
+    * CSS
+    * JQuery
+    * Bootstrap
+    * Moment.JS
+    * REST API
+## Website
+ https://kdchurch1994.github.io/weather-dashboard/
+
+ ## Image
+![Alt text](./assets/images/weather-dashboard.JPG)
+ ## Contribution
+ Made By Kyle Church
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     setInterval(update, 1000);
 });
 
-function generateWeather() {
+function generateWeather() { //This function is being used to generate the current weather for the searched city and five day forecast by setting the const to get a particular element from the html by use getElementbyID
     const cityInputEl = document.getElementById("input-city");
     const citySearchEl = document.getElementById("btn-search");
     const clearCityEl = document.getElementById("history-clear");
@@ -25,11 +25,11 @@ function generateWeather() {
     const cityPictureEl = document.getElementById("city-pic");
     const cityWindEl = document.getElementById("wind-speed");
     const searchHistoryEl = document.getElementById("search-history");
-    var fiveDayEl = document.getElementById("fiveday-header");
+    var fiveDayEl = document.getElementById("fiveday-header"); 
     var todayweatherEl = document.getElementById("todays-weather");
-    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || []; //creates local storarge by parsing the searched cities or creating an empty array if no cities have been searched
 
-    //Function that displays the temperature with degress fahrenheit
+    //Function that displays the correct temperature with degrees fahrenheit
     function k2f(K) {
         return Math.floor((K - 273.15) * 1.8 + 32);
     }
@@ -37,7 +37,7 @@ function generateWeather() {
     //Assigning a variable to use as the API Key
     const APIkey = "04d672453e2ffd5f0ccd96121758e383";
 
-    function fetchWeather(cityName) {
+    function fetchWeather(cityName) { //fetches the weather based off the name of a city
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
         fetch(queryURL)
             .then(function (response) {
@@ -45,21 +45,21 @@ function generateWeather() {
             })
             .then(function (response) {
                 console.log(response)
-                todayweatherEl.classList.remove("d-none");
+                todayweatherEl.classList.remove("d-none"); //removes the d-none classification for the city cards so that this information can be viewed on the webpage
 
-                const currentDate = new Date(response.dt * 1000);
+                const currentDate = new Date(response.dt * 1000); //gets the date to display on the weather card
                 const day = currentDate.getDate();
                 const month = currentDate.getMonth() + 1;
                 const year = currentDate.getFullYear();
                 nameEl.innerHTML = response.name + " (" + month + "/" + day + "/" + year + ") ";
                 let weatherPic = response.weather[0].icon;
-                cityPictureEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                cityPictureEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png"); //Pulls the picture from the api and displays it on the weather card
                 cityPictureEl.setAttribute("alt", response.weather[0].description); 
-                cityTemperatureEl.innerHTML = "Temperature: " + k2f(response.main.temp) + " &#176F";
-                cityHumidityEl.innerHTML = "Humidity: " + response.main.humidity + "%";
-                cityWindEl.innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
+                cityTemperatureEl.innerHTML = "Temperature: " + k2f(response.main.temp) + " &#176F"; //Pulls the temperature for the searched city from the api and adds the k2f function to display the temperature with the correct degrees Farenheidt
+                cityHumidityEl.innerHTML = "Humidity: " + response.main.humidity + "%"; //Pulls the humidty for the searched city from the api and formats it for how it will be displayed on the webpage
+                cityWindEl.innerHTML = "Wind Speed: " + response.wind.speed + " MPH"; //Pulls the wind speed for the searched city from the api and formats it for how it will be displayed on the webpage
 
-                //Retrieve UV Index
+                //Retrieve UV Index 
                 let lat = response.coord.lat;
                 let lon = response.coord.lon;
                 let UVQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey + "&cnt=1";
@@ -93,11 +93,11 @@ function generateWeather() {
                 let forecastQueryURL =  "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIkey;
                 fetch(forecastQueryURL)
 
-                    .then( function (response) {
+                    .then( function (response) { //Takes the fetched data and converts it to json
                         return response.json()
                     })
                     .then(function (response) {
-                        fiveDayEl.classList.remove("d-none");
+                        fiveDayEl.classList.remove("d-none"); //Allows us to see the 5 day forcast by removing the d-none class from the fiveday-header
                         console.log(response)
 
                         // This code includes a for loop that will parse the previous response data and provide the 5 day forecast
@@ -105,7 +105,7 @@ function generateWeather() {
                         for (i =0; i < weatherEls.length; i++) {
                             weatherEls[i].innerHTML = "";
                             const weatherIndex = i * 8 + 4;
-                            const weatherDate = new Date (response.list[weatherIndex].dt * 1000);
+                            const weatherDate = new Date (response.list[weatherIndex].dt * 1000); //gets the date to display on the weather card for each of the five days
                             const weatherDay = weatherDate.getDate();
                             const weatherMonth = weatherDate.getMonth() + 1;
                             const weatherYear = weatherDate.getFullYear();
@@ -129,7 +129,8 @@ function generateWeather() {
                             const weatherHumidityEl = document.createElement("p");
                             weatherHumidityEl.innerHTML = "Humidity: " + response.list[weatherIndex].main.humidity + "%";
                             weatherEls[i].append(weatherHumidityEl);
-
+                            
+                            //add element to display the windspeed for each day
                             const weatherWindEl = document.createElement("p");
                             weatherWindEl.innerHTML = "Wind Speed: " + response.list[weatherIndex].wind.speed + " MPH";
                             weatherEls[i].append(weatherWindEl);
